c9cb2e265fb3e727fc43966287157bf0bad9c359
DZOOOOO <dzpro0327@naver.com>
2023-12-26 18:52:32 +0900

    member servlet add

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,54 @@
+package hello.servlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    // 데이터 클리닝 안해주면 문제가 생김 --> 실행 순서의 보장이 없다.
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    @DisplayName("")
+    void save() {
+        // given
+        Member member = new Member("hello", 20);
+
+        // when
+        Member saveMember = memberRepository.save(member);
+
+        // then
+        Member findMember = memberRepository.findById(saveMember.getId());
+        assertThat(findMember).isEqualTo(saveMember);
+    }
+
+    @Test
+    @DisplayName("")
+    void findAll() {
+        // given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // when
+        List<Member> result = memberRepository.findAll();
+
+        // then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+}
